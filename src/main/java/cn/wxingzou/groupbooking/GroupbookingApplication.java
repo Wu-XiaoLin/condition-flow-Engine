e220e6da9c11cdafa6304000a6386399f50e5462
wuxiaolin <1132507247@qq.com>
2019-11-08 15:54:18 +0800

    Initial commit

---
+++ src/main/java/cn/wxingzou/groupbooking/GroupbookingApplication.java
@@ -0,0 +1,13 @@
+package cn.wxingzou.groupbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupbookingApplication.class, args);
+    }
+
+}
