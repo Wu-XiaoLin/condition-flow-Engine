011be8f504f46e605b95da819da9f73a04d359ca
““wuxiaoli <wxl142528@163.com>
2019-12-18 23:59:17 +0800

    2.0 新增条件链全局参数

--- src/main/java/cn/wxingzou/conditionflow/core/Condition.java
+++ src/main/java/cn/wxingzou/conditionflow/core/Condition.java
@@ -2,6 +2,8 @@ package cn.wxingzou.conditionflow.core;
 
 import cn.wxingzou.conditionflow.result.Result;
 
+import java.util.Map;
+
 /**
  * 条件定义
  *
@@ -14,13 +16,15 @@ public interface Condition<T> {
     /**
      * 执行条件内的检查
      *
-     * @param t 入参自己定义
+     * @param properties 条件链所属的全局参数（只读，只读，只读）
+     * @param t          入参自己定义
      * @return
      */
-    Result check(T t);
+    Result check(Map<String, Object> properties, T t);
 
     /**
      * 获取条件的描述
+     *
      * @return
      */
     String getDescription();
